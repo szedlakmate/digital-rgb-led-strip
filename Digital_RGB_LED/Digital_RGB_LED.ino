7297347eb5fccbf4f5003ca4f5a1581002ad0eed
Mate Szedlak <szedlakmate@gmail.com>
2020-12-28 22:15:57 +0000

    Setup WS2812B LED stripe

--- Digital_RGB_LED/Digital_RGB_LED.ino
+++ Digital_RGB_LED/Digital_RGB_LED.ino
@@ -2,10 +2,12 @@
 
 #define LED_PIN     12
 #define NUM_LEDS    300
-#define BRIGHTNESS  50
+#define BRIGHTNESS  150
 #define LED_TYPE    WS2812B
-#define COLOR_ORDER BRG
+#define COLOR_ORDER GRB
 CRGB leds[NUM_LEDS];
+
+
 
 #define UPDATES_PER_SECOND 10
 // This example shows several ways to set up and use 'palettes' of colors
@@ -29,21 +31,18 @@ CRGB leds[NUM_LEDS];
 
 CRGBPalette16 currentPalette;
 TBlendType    currentBlending;
-
-extern CRGBPalette16 myRedWhiteBluePalette;
-extern const TProgmemPalette16 myRedWhiteBluePalette_p PROGMEM;
-
 static uint8_t startIndex = 0;
+static bool reversed = true;
 
 void setup() {
     delay( 500 ); // power-up safety delay
-    FastLED.addLeds<LED_TYPE, LED_PIN, COLOR_ORDER>(leds, NUM_LEDS).setCorrection( TypicalLEDStrip );
+    FastLED.addLeds<LED_TYPE, LED_PIN, COLOR_ORDER>(leds, NUM_LEDS).setCorrection( 0x7050FF ); // TypicalLEDStrip
     FastLED.setBrightness(  BRIGHTNESS );
     
     currentBlending = LINEARBLEND;
 
     // OceanColors_p, CloudColors_p, LavaColors_p, HeatColors_p, ForestColors_p, and PartyColors_p., RainbowColors_p, RainbowStripeColors_p 
-     currentPalette = RainbowStripeColors_p ;
+     currentPalette = RainbowColors_p ;
   
 }
 
@@ -64,11 +63,14 @@ void loop()
 
 void FillLEDsFromPaletteColors( uint8_t colorIndex)
 {
-    uint8_t brightness = 255;
-    
     for( int i = 0; i < NUM_LEDS; i++) {
-        leds[i] = ColorFromPalette( currentPalette, colorIndex, brightness, currentBlending);
-        colorIndex += 1;
+      int index = i;
+      if (reversed) {
+        index = NUM_LEDS - i -1;
+      }
+        leds[index] = ColorFromPalette( currentPalette, colorIndex, BRIGHTNESS, currentBlending);
+        // TODO: This is bad. indexing is complety wrong.
+        colorIndex +=1;
     }
 }
 
@@ -77,38 +79,8 @@ void FillLEDsFromPaletteColors( uint8_t colorIndex)
 //
 // FastLED provides several 'preset' palettes: RainbowColors_p, RainbowStripeColors_p,
 // OceanColors_p, CloudColors_p, LavaColors_p, ForestColors_p, and PartyColors_p.
-//
-// Additionally, you can manually define your own color palettes, or you can write
-// code that creates color palettes on the fly.  All are shown here.
 
-void ChangePalettePeriodically()
-{
-    uint8_t secondHand = (millis() / 1000) % 60;
-    static uint8_t lastSecond = 99;
-    
-    if( lastSecond != secondHand) {
-        lastSecond = secondHand;
-        if( secondHand ==  0)  { currentPalette = RainbowColors_p;         currentBlending = LINEARBLEND; }
-        if( secondHand == 10)  { currentPalette = RainbowStripeColors_p;   currentBlending = NOBLEND;  }
-        if( secondHand == 15)  { currentPalette = RainbowStripeColors_p;   currentBlending = LINEARBLEND; }
-        if( secondHand == 20)  { SetupPurpleAndGreenPalette();             currentBlending = LINEARBLEND; }
-        if( secondHand == 25)  { SetupTotallyRandomPalette();              currentBlending = LINEARBLEND; }
-        if( secondHand == 30)  { SetupBlackAndWhiteStripedPalette();       currentBlending = NOBLEND; }
-        if( secondHand == 35)  { SetupBlackAndWhiteStripedPalette();       currentBlending = LINEARBLEND; }
-        if( secondHand == 40)  { currentPalette = CloudColors_p;           currentBlending = LINEARBLEND; }
-        if( secondHand == 45)  { currentPalette = PartyColors_p;           currentBlending = LINEARBLEND; }
-        if( secondHand == 50)  { currentPalette = myRedWhiteBluePalette_p; currentBlending = NOBLEND;  }
-        if( secondHand == 55)  { currentPalette = myRedWhiteBluePalette_p; currentBlending = LINEARBLEND; }
-    }
-}
 
-// This function fills the palette with totally random colors.
-void SetupTotallyRandomPalette()
-{
-    for( int i = 0; i < 16; i++) {
-        currentPalette[i] = CHSV( random8(), 255, random8());
-    }
-}
 /*
 void SetColorPalette(String color)
 {
