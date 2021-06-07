113e8e64b2d3d4198b9316a1cbb8405b1b77dcd2
Mate Szedlak <szedlakmate@gmail.com>
2021-06-07 21:59:00 +0100

    Add ultrasound measurement

--- Digital_RGB_LED/Digital_RGB_LED.ino
+++ Digital_RGB_LED/Digital_RGB_LED.ino
@@ -2,20 +2,41 @@
 
 #define LED_PIN     53
 #define NUM_LEDS    300
-#define BRIGHTNESS  250 // max: 250
+#define BRIGHTNESS  100 // max: 250
 #define LED_TYPE    WS2812B
 #define COLOR_ORDER GRB
 CRGB leds[NUM_LEDS];
 
+// Inject ultrasound distance measurement from https://github.com/szedlakmate/arduino-ultrasound-distance-measurement
+
+// Pins
+const int TRIG_PIN = 7;   // Ultrasound device 
+const int ECHO_PIN = 8;   // Ultrasound device  
+
+const int numReadings = 3;
+const float threshold = 100;     // ?unit
+
+float readings[numReadings];    // the readings from the analog input
+int readIndex = 0;              // the index of the current reading
+float sum = 0;                  // sum of readings
+float average = 0;              // the average
+float prevavg = 0;              // previous average
+int count = 0;                  // counting readings' elements
+int dropcount = 0;              // counting the measurement anomalies
+
+// Anything over 50 cm (2900 us(?) pulse) is "out of range"
+const unsigned int MAX_DIST = 3300;
+
+// *** End of injection *********************************
 
 // Wave per miniute. 1 means it takes 60 sec to flow through each LEDs
 // Max BPM is ~10 for 300 LEDS (RESOLUTION=1) 
 // Max BPM*RESOLUTION is ~3 for 300 LEDS (RESOLUTION>1) 
-#define BPM 2.0
+#define BPM 0.5
 
 // Advised max (sub) RESOLUTION is ~3, Min 1, Default 1
 // *** Set to 1 to reach FAST animation
-#define RESOLUTION 1
+#define RESOLUTION 5
 
 // Scales the wave's length. >1.0 means overlays the stripe. Default: 1.0
 #define WAVE_LENGTH_SCALE 1.00
@@ -63,8 +84,22 @@ void setup() {
      // Initialize LED colors
      FillLEDsFromPaletteColors(startIndex, false);
 
-     // Turning ON Serial communication may slow down the animation
-//     Serial.begin(9600); 
+
+    // Ultrasound stuff ***********
+   
+    // The Trigger pin will tell the sensor to range find
+    pinMode(TRIG_PIN, OUTPUT);
+    digitalWrite(TRIG_PIN, LOW);
+  
+    // Initialize built-in LED - Blinking signs active data transfer
+    pinMode(LED_BUILTIN, OUTPUT);
+    digitalWrite(LED_BUILTIN, LOW);           // Set built-in LED to default: Off  
+  
+    // We'll use the serial monitor to view the sensor output
+    Serial.begin(9600);
+    for (int thisReading = 0; thisReading < numReadings; thisReading++) {
+      readings[thisReading] = 0;  
+    }
 }
 
 
@@ -73,6 +108,14 @@ void loop()
     FillLEDsFromPaletteColors(startIndex, true);
 
     // SetColorPalette(CRGB::Red);
+
+    // Ultrasound measurement
+    measure();
+    Serial.println(average);
+    if (average > 0) {
+      FastLED.setBrightness( min( average, 250) );
+    }
+  
     
     FastLED.show();
     startIndex = startIndex + 1;
@@ -84,6 +127,90 @@ void loop()
 //    Serial.println(delayDelta - timer + stopper);
     stopper = timer;
 }
+
+// Ultrasonic mess *****************************
+void reset(){
+  // If too many highly different measurement were recorded, 
+  // the program drops all saved data and reset the variables.
+  for (int thisReading = 0; thisReading < numReadings; thisReading++) {
+    readings[thisReading] = 0; 
+  }
+  count = 0;
+  readIndex = 0;
+  sum = 0;
+  average = 0;
+  dropcount = 0;
+  prevavg = 0;
+}
+
+
+void measure() {
+   unsigned long t1;
+  unsigned long t2;
+  unsigned long pulse_width;
+  float mm;
+
+  // Hold the trigger pin high for at least 10 us
+  digitalWrite(TRIG_PIN, HIGH);
+  delayMicroseconds(10);
+  digitalWrite(TRIG_PIN, LOW);
+
+  // Wait for pulse on echo pin
+  while ( digitalRead(ECHO_PIN) == 0 );
+
+  // Measure how long the echo pin was held high (pulse width)
+  // Note: the micros() counter will overflow after ~70 min
+  t1 = micros();
+  while ( digitalRead(ECHO_PIN) == 1);
+  t2 = micros();
+  pulse_width = t2 - t1;
+
+                   // Calculate distance in millimeters. The constants
+                   // are found in the datasheet, and calculated from the assumed speed 
+                   // of sound in air at sea level (~340 m/s).
+                   // mm = round(pulse_width / 0.580)/10; // Original calculation method
+  mm = round(pulse_width *1.716+6.76)/10; // Calibrated method
+  // Print out results
+  if ( pulse_width > MAX_DIST ) {
+//     Serial.println("Out of range");
+  } else {
+      if ((count < numReadings) or ((mm > average - threshold) and (mm < average + threshold))) {
+        if (count < numReadings) {
+          count += 1;
+          } 
+         // drop old data
+        sum = sum - readings[readIndex];
+        // add new data
+        readings[readIndex] = mm;
+        sum = sum + readings[readIndex];
+        readIndex = readIndex + 1;
+        if (readIndex >= numReadings) {
+          // ...wrap around to the beginning:
+          readIndex = 0;
+          if (dropcount > 0) {
+            dropcount -= 1;
+          }
+        }
+        // Calcualting the average
+        average = sum / numReadings;
+        if ((count == numReadings) and (average < prevavg + threshold)
+        and (average > prevavg - threshold)) {
+//          Serial.print(average);               
+//          Serial.println(",");
+        }
+        prevavg = average;
+      } else {
+        if (count == numReadings){
+          dropcount += 1;
+          if (dropcount == 3){
+            reset();
+          }
+        }
+      }
+  }
+  
+}
+
 
 void FillLEDsFromPaletteColors(long colorShift, bool shiftOnly)
 {
