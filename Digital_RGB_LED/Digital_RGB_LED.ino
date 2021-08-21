bf97c7266924e5fe7eb33504bbd548253e950358
Mate Szedlak <szedlakmate@gmail.com>
2021-08-21 16:09:08 +0100

    Reset mySunsetPeach palette

--- Digital_RGB_LED/Digital_RGB_LED.ino
+++ Digital_RGB_LED/Digital_RGB_LED.ino
@@ -104,25 +104,25 @@ const TProgmemPalette16 mySunsetPurple PROGMEM =
 
 const TProgmemPalette16 mySunsetPeach PROGMEM =
 {
-    0x731c94,
-    0x892b88,
-    CRGB(159, 57,125),
-    CRGB(178, 69,115),
-
-    CRGB(194, 80,106),
-    CRGB(207, 88,100),
-    CRGB(214, 93, 96),
-    CRGB(217, 94, 94),
+    0x721c93,
+    0x892a87,
+    0x9f387c,
+    0xb24572,
   
-    CRGB(217, 94, 94),
-    CRGB(214, 93, 96),
-    CRGB(207, 88,100),
-    CRGB(194, 80,106),
+    0xc24f6a,
+    0xce5763,
+    0xd65c5f,
+    0xd85e5e,
     
-    CRGB(178, 69,115),
-    CRGB(159, 57,125),
-    0x892b88,
-    0x731c94,
+    0xd85e5e,
+    0xd65c5f,
+    0xce5763,
+    0xc24f6a,
+
+    0xb24572,
+    0x9f387c,
+    0x892a87,
+    0x721c93,
 };
 
 const TProgmemPalette16 mySunsetGreen PROGMEM =
@@ -183,10 +183,10 @@ void setup() {
     // OceanColors_p, CloudColors_p, LavaColors_p, HeatColors_p, ForestColors_p, and PartyColors_p., RainbowColors_p, RainbowStripeColors_p 
 
     // mySunsetBlue, mySunsetPurple, mySunsetPeach, mySunsetGreen
-//     currentPalette = mySunsetPeach;
-      currentPalette = DebugPalette;
-//     newPalette = RainbowColors_p;
-      newPalette = DebugPalette;
+     currentPalette = mySunsetPeach;
+//      currentPalette = DebugPalette;
+     newPalette = mySunsetPeach;
+//      newPalette = DebugPalette;
 //     fill_solid( newPalette, 16, CRGB::Red);
 
 //      newPalette =  currentPalette;
