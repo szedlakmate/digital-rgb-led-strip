cd0dc812b2a4ebcf733272849b9edce059f67527
Szedlák Máté <szedlakmate@gmail.com>
2022-11-18 20:42:48 +0000

    Add potmeters Brightness - Speed - WaveLength

--- Digital_RGB_LED/Digital_RGB_LED.ino
+++ Digital_RGB_LED/Digital_RGB_LED.ino
@@ -4,40 +4,60 @@
  version 2 as published by the Free Software Foundation.
  ***************/
 
-
+// Original source: https://www.instructables.com/Tutorial-How-to-4-Digit-Display-Interface-With-Ard/
 #include <FastLED.h>
+#include <TM1637Display.h>
+
+#define LED_TYPE WS2812B
+#define COLOR_ORDER GRB
+
+// Module connection pins (Digital Pins)
+#define CLK 2
+#define DIO 3
+TM1637Display display(CLK, DIO);
 
 // Pins
 #define LED_PIN 25  // arduino digital pin
+#define BRIGHTNESS_INPUT_PIN 1
+#define WAVE_LENGTH_INPUT_PIN 3
+#define BPM_INPUT_PIN 2
 
-#define NUM_LEDS 300    // total num of leds on the full strip
-#define BRIGHTNESS 120  // max: 250
-#define LED_TYPE WS2812B
-#define COLOR_ORDER GRB
+#define NUM_LEDS 300  // total num of leds on the full strip
+
+int BRIGHTNESS = 120;  // max: 250
+
+#define FILTER_FACTOR 3  // Defines how much measurements needed to be considered to apply change
 
 CRGB leds[NUM_LEDS];
 CRGB ledsPreset[NUM_LEDS];
 
 // Wave per miniute. 1 means it takes 60 sec to flow through each LEDs
 // Max BPM is ~10 for 300 LEDS
-#define BPM 2.0
+float BPM = 2.0;
+#define BPM_MAX 15.0
 
 // Scales the wave's length. >1.0 means overlays the stripe. Default: 1.0
-#define WAVE_LENGTH_SCALE 1.00
+float WAVE_LENGTH_SCALE = 1.00;
+#define WAVE_LENGTH_SCALE_MAX 5.0
 
 CRGBPalette256 currentPalette;
 TBlendType currentBlending;
 
 static long looper = 0;
 
+
+int determineDelayMillis() {
+  return (60000.0 * 2.0) / ((float)NUM_LEDS * max(BPM, 0.00000000001));
+}
+
 // Determine delay time based on BPM
-static int delayMillis = (60000.0 * 2.0) / ((float)NUM_LEDS * BPM);
+static int delayMillis = determineDelayMillis();
 long stopper = millis();
 
 
 void setup() {
   delay(500);  // power-up safety delay
-  Serial.begin(1000000);
+  Serial.begin(500000);
   Serial.println("START");
 
   FastLED.addLeds<LED_TYPE, LED_PIN, COLOR_ORDER>(leds, NUM_LEDS).setCorrection(TypicalLEDStrip);
@@ -48,7 +68,11 @@ void setup() {
   // OceanColors_p, CloudColors_p, LavaColors_p, HeatColors_p, ForestColors_p, and PartyColors_p., RainbowColors_p, RainbowStripeColors_p
   currentPalette = RainbowColors_p;
 
-  InitStripe();
+  measureAndApplySatet();
+
+  display.setBrightness(0x08);
+
+  FillLEDsFromPaletteColors(0);
 }
 
 
@@ -66,23 +90,114 @@ void loop() {
     Serial.print("Missed [ms]:   ");
     Serial.println(-(delayMillis - now + stopper));
   }
-  FastLED.delay(waitMoreMillis);
+
+  waitAndMeasure(waitMoreMillis);
+
+  showBPM(waitMoreMillis > 0);
   stopper = now;
 }
 
-void InitStripe() {
-  for (int i = 0; i < NUM_LEDS; i += 1) {
-    float colorIndex = (float)i / (WAVE_LENGTH_SCALE * (float)NUM_LEDS) * 256.0;
+void waitAndMeasure(int waitMillis) {
+  int samplingCount = max(round(waitMillis / 300.0), 1);
 
-    ledsPreset[i] = ColorFromPalette(currentPalette, colorIndex, BRIGHTNESS, currentBlending);
+  if (samplingCount > 10) {
+    Serial.print("!! samplingCount:   ");
+    Serial.println(samplingCount);
+  }
+
+  for (int i = 0; i < samplingCount; i += 1) {
+    FastLED.delay(waitMillis / samplingCount);
+    measureAndApplySatet();
+
+    if (BPM == 0.0) {
+      i = 0;
+    }
+
+    if (determineDelayMillis() < waitMillis * i / samplingCount && BPM > 0.0) {
+      break;
+    }
   }
 }
 
 void FillLEDsFromPaletteColors(int looper) {
   for (int i = 0; i < NUM_LEDS; i++) {
-    int index = looper + i;
-    leds[i] = ledsPreset[index % NUM_LEDS];
+    float colorIndex = (float)(i + looper) / (WAVE_LENGTH_SCALE * (float)NUM_LEDS) * 256.0;
+
+    leds[i] = ColorFromPalette(currentPalette, colorIndex, BRIGHTNESS, currentBlending);
+    //leds[i] = ledsPreset[index % NUM_LEDS];
   }
+}
+
+void measureAndApplySatet() {
+  //setBrightnessByPotmeter(analogRead(BRIGHTNESS_INPUT_PIN));
+  setBpmByPotmeter(analogRead(BPM_INPUT_PIN));
+  setWaveLengthByPotmeter(analogRead(WAVE_LENGTH_INPUT_PIN));
+}
+
+int determineBrightness(long potMeterValue) {
+  return potMeterValue; // potMeterValue/255*255
+}
+
+float determineBPM(long potMeterValue) {
+  return max((potMeterValue / 255.0) * BPM_MAX, 0);
+}
+
+float determineWaveLength(long potMeterValue) {
+  float rawWaveLength = max(round(potMeterValue / 5.0 ) / 51.0 * WAVE_LENGTH_SCALE_MAX, 0.05);
+  // Filter noise by rounding scaled value. Helps at very short wave lengths
+  return ((float)round(rawWaveLength * 50)) / 50.0;
+}
+
+void setBrightnessByPotmeter(long potMeterValue) {
+  Serial.print("  potMeterValue: ");
+  Serial.println(potMeterValue);
+  BRIGHTNESS += (determineBrightness(potMeterValue) - BRIGHTNESS) / FILTER_FACTOR;
+  Serial.print("  BRIGHTNESS: ");
+  Serial.println(BRIGHTNESS);
+  FastLED.setBrightness(BRIGHTNESS);
+}
+
+void setBpmByPotmeter(float potMeterValue) {
+  //Serial.print("  potMeterValue: ");
+  //Serial.println(potMeterValue);
+  BPM += (determineBPM(potMeterValue) - BPM) / FILTER_FACTOR;
+  Serial.print("  BPM: ");
+  Serial.println(BPM);
+  delayMillis = determineDelayMillis();
+}
+
+void setWaveLengthByPotmeter(float potMeterValue) {
+  Serial.print("  potMeterValue: ");
+  Serial.println(potMeterValue);
+  WAVE_LENGTH_SCALE += (determineWaveLength(potMeterValue) - WAVE_LENGTH_SCALE) / FILTER_FACTOR;
+
+  Serial.print("  WAVE_LENGTH_SCALE: ");
+  Serial.println(WAVE_LENGTH_SCALE);
+}
+
+
+float showBPM(bool bpmIsValid) {
+  // Set segment display
+  uint8_t data[] = { 0x00, 0x80, 0x00, 0x00 };
+
+  // Selectively set different digits
+  data[3] += display.encodeDigit((int)(BPM * 100) % 10);
+
+  if (BPM > 0.1) {
+    data[2] += display.encodeDigit((int)(BPM * 10) % 10);
+  }
+  if (BPM > 1) {
+    data[1] += display.encodeDigit((int)BPM % 10);
+  }
+  if (BPM > 10) {
+    data[0] += display.encodeDigit((int)(BPM / 10) % 10);
+  }
+
+  if (!bpmIsValid) {
+    data[0] = 0x49;
+  }
+
+  display.setSegments(data);
 }
 
 /*******************************************************
@@ -189,7 +304,7 @@ const TProgmemPalette16 myRedWhiteBluePalette_p PROGMEM = {
 // between the 16 explicit entries to create fifteen intermediate palette
 // entries between each pair.
 //
-// So for example, if you set the first two explicit entries of a compact 
-// palette to Green (0,255,0) and Blue (0,0,255), and then retrieved 
+// So for example, if you set the first two explicit entries of a compact
+// palette to Green (0,255,0) and Blue (0,0,255), and then retrieved
 // the first sixteen entries from the virtual palette (of 256), you'd get
 // Green, followed by a smooth gradient from green-to-blue, and then Blue.
