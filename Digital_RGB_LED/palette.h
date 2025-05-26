c842354209b62d60bae16cc4c20803a28063b600
Mate Szedlak <szedlakmate@gmail.com>
2025-05-26 13:44:35 -0300

    *** SHOULD FAIL ***

--- Digital_RGB_LED/palette.h
+++ Digital_RGB_LED/palette.h
@@ -3,6 +3,8 @@
 
 #include <FastLED.h>
 
+aranygaluska
+
 void SetupBlackAndWhiteStripedPalette();
 void SetupPurpleAndGreenPalette();
 extern const TProgmemPalette16 myRedWhiteBluePalette_p PROGMEM;
