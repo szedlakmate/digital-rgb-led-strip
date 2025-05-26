380477eb47b467715aed5588ba537b0a89cf86de
Mate Szedlak <szedlakmate@gmail.com>
2025-05-26 14:37:29 -0300

    Reset LED config

--- Digital_RGB_LED/config.h
+++ Digital_RGB_LED/config.h
@@ -6,15 +6,15 @@
 // Pins
 #define LED_PIN 31  // arduino digital pin
 #define NUM_LEDS 300    // total num of leds blocks on the strip
-#define BRIGHTNESS 150  // max: 250
+#define BRIGHTNESS 220  // max: 250
 #define LED_TYPE WS2812B
 #define COLOR_ORDER GRB
 
 // Animation parameters
-#define BPM 7.5
-#define WAVE_LENGTH_SCALE 0.7
+#define BPM 5.0
+#define WAVE_LENGTH_SCALE 1.0
 #define RESOLUTION 1
-#define REVERSED true
+#define REVERSED false
 
 extern CRGB leds[NUM_LEDS];
 extern CRGBPalette256 currentPalette;
