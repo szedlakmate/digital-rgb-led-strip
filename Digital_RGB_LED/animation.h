e5838aaa73654c060df6c882c42a776e397ab98e
Máté Sedlák <szedlakmate@gmail.com>
2025-05-26 13:07:57 -0300

    Split code (#11)
    * Add Jetbrains config
    
    * Split code

---
+++ Digital_RGB_LED/animation.h
@@ -0,0 +1,9 @@
+#ifndef ANIMATION_H
+#define ANIMATION_H
+
+#include <FastLED.h>
+
+void FillLEDsFromPaletteColors(long colorShift);
+
+#endif // ANIMATION_H
+
---
+++ Digital_RGB_LED/palette.h
@@ -0,0 +1,11 @@
+#ifndef PALETTE_H
+#define PALETTE_H
+
+#include <FastLED.h>
+
+void SetupBlackAndWhiteStripedPalette();
+void SetupPurpleAndGreenPalette();
+extern const TProgmemPalette16 myRedWhiteBluePalette_p PROGMEM;
+
+#endif // PALETTE_H
+
